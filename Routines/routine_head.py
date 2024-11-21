7dad01c652645912f41eeffce58485d17ad99635
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2024-11-21 17:04:48 +0100

    Misc trials on centering (to try and see antennas movements)

---
+++ Routines/routine_head.py
@@ -0,0 +1,111 @@
+'''
+IP routine
+'''
+
+import os
+import numpy as np
+import pandas as pd
+import cv2 as cv
+from alive_progress import alive_bar
+import matplotlib.pyplot as plt
+
+import IP
+
+os.system('clear')
+
+# === Parameters ===========================================================
+
+stype = 'Single'      # 'Single' / 'Social'
+btype = 'foragers'    # 'foragers' / 'nurses'
+
+bsize = 100
+
+# ==========================================================================
+
+# Data handler
+
+H = IP.handler(stype, btype)
+
+row = H.df.iloc[0]
+
+print(row)
+movie_code = row['video code']
+dish = row['petri dish place']
+pix2mm = row['pix2mm']
+
+P = IP.processor(H.type, movie_code, dish, pix2mm=pix2mm)
+
+# Trajectory
+df = pd.read_csv(P.file['traj'])
+
+# print(df.y[0])
+
+# === Image processing =====================================================
+
+plt.style.use('dark_background')
+plt.rcParams.update({'font.size': 22})
+fig, ax = plt.subplots(1,1, figsize=(20,20))
+
+# Input video 
+cap = cv.VideoCapture(P.file['movie']['path'])
+
+with alive_bar(P.param['T']-1) as bar:
+
+  frame = 0
+  t = 0
+  id = 0
+  Eref = None
+
+  bar.title(P.file['movie']['filename'][-14:-4])
+  
+  while cap.isOpened():
+
+    Img = P.get_frame(cap)
+    if Img is None: break
+  
+    # --- Processing ---------------------------------------------------
+
+    dx = df.x[frame]/P.param['pix2mm']
+    dy = df.y[frame]/P.param['pix2mm']
+    
+    M = cv.getRotationMatrix2D((dx, dy), df.theta[frame]*180/np.pi+90, 1)
+    M[0,2] += bsize/2 - dx
+    M[1,2] += bsize/2 - dy
+
+    Img = P.background - Img
+    Res = cv.warpAffine(Img, M, [bsize]*2)
+    
+    # --- Display -------------------------------------------------------
+    
+    # Determine head
+    # # # b2 = int(bsize/2)
+    # # # R0 = np.sum(Res[0:b2, :])
+    # # # R1 = np.sum(Res[b2:,:])
+    # # # if R0<R1:
+    # # #   Res = np.flip(Res, axis=0)
+
+    if frame==0:
+      ax.imshow(Res, cmap='gray', vmax=0.1)
+      # ax.imshow(np.log(Res), cmap='gray')
+      plt.show()
+      break
+
+    # Upscale
+    Res = cv.resize(Res, (500,500), interpolation=cv.INTER_LINEAR)
+
+    # Display
+    cv.imshow('frame', Res)
+
+    if cv.waitKey(1) == ord('q'):
+      break
+
+    # --- Update
+
+    frame += 1
+    t = frame/P.param['fps']
+    bar()
+
+#  --- End -------------------------------------------------------------
+
+cap.release()
+cv.destroyAllWindows()
